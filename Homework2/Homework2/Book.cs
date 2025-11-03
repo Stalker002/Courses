f7665a15455aa84c25ea40a0af9365c20c7277c4
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2025-11-03 18:38:33 +0300

    Reapply "Add homework"
    This reverts commit 7e13472e25b78af30e47e5aa350172b165ee2aa4.

---
+++ Homework2/Homework2/Book.cs
@@ -0,0 +1,62 @@
+﻿namespace Task1;
+
+internal class Book
+{
+    private string _author = "Unknow";
+    private string _title = "Unknow";
+    private int _page = 0;
+
+    public string Author
+    {
+        get { return _author; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(_author))
+            {
+                throw new ArgumentException();
+            }
+
+            _author = value;
+        }
+    }
+
+    public string Title
+    {
+        get
+        {
+            return _title ;
+        }
+        set
+        {
+            _title = value;
+        }
+    }
+
+    public int Page
+    {
+        get
+        {
+            return _page;
+        }
+        set
+        {
+            _page = value;
+        }
+    }
+
+    public Book(string author, string title, int page)
+    {
+        Author = author;
+        Title = title;
+        Page = page;
+    }
+
+    public Book()
+    {
+    }
+
+    public void GetDescription()
+    {
+        Console.WriteLine($"Название: {Title}, Автор: {Author}, Кол-во страниц: {Page}");
+    }
+}
