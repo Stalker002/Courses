75b2f858d01ecbfec9279048db7fd94bf5cab7de
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2026-01-22 17:11:08 +0300

    Did third homework

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework3;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var worker = new Worker("Бен", "Слесарь", "+375465675658");
+        worker.GetDetails();
+        EmployeeRegistry.AddEmployee(worker);
+
+        var manager = new Manager("Михаил", "Менеджер", "michail@gmail.com");
+        manager.GetDetails();
+        EmployeeRegistry.AddEmployee(manager);
+
+        EmployeeRegistry.ListAllEmployees();
+
+        EmployeeRegistry.FindEmployee("МИхаил");
+
+        var project = new Manager.Project("Дорога", "18 января");
+        project.DisplayProjectInfo();
+    }
+}
