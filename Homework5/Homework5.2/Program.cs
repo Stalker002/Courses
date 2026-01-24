c8de3421f599c14f4c4d674a31cdbe2ba86296e7
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2026-01-24 18:54:03 +0300

    Add fifth homework

---
+++ Homework5/Homework5.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace Homework5._2;
+
+public static class Program
+{
+    private static void Main()
+    {
+        Console.Write("Введите строку: ");
+        var str = Console.ReadLine();
+
+        var sb = new StringBuilder(str.Length);
+        foreach (var ch in str)
+        {
+            if (!char.IsWhiteSpace(ch))
+                sb.Append(ch);
+        }
+
+        var noSpace = sb.ToString();
+        Console.WriteLine($"Без пробелов: {noSpace}");
+
+        var upper = str.ToUpper();
+        Console.WriteLine($"Все заглавные: {upper}");
+
+        var lower = str.ToLower();
+        Console.WriteLine($"Все строчные: {lower}");
+
+        var init = new StringBuilder(str.Length);
+        bool newWord = true;
+        foreach (var ch in str)
+        {
+            if (char.IsLetterOrDigit(ch))
+            {
+                if (newWord)
+                    init.Append(char.ToUpper(ch));
+                else
+                    init.Append(char.ToLower(ch));
+
+                newWord = false;
+            }
+            else
+            {
+                init.Append(ch);
+                newWord = true;
+            }
+        }
+
+        var initText = init.ToString();
+        Console.WriteLine($"Первая буква каждого слова в заглавной: {initText}");
+    }
+}
---
+++ Homework5/Homework5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Homework5;
+
+public static class Program
+{
+    private static void Main()
+    {
+        Console.Write("Введите строку: ");
+        var str = Console.ReadLine();
+        var sb = new StringBuilder(str.Length);
+        foreach (var ch in str)
+        {
+            if (char.IsLetterOrDigit(ch))
+                sb.Append(char.ToLowerInvariant(ch));
+        }
+
+        int i = 0, j = sb.Length - 1;
+        var res = false;
+        while (i < j)
+        {
+            if (sb[i] == sb[j])
+                res = true;
+            i++; j--;
+        }
+
+        Console.WriteLine(res ? "Строка является палиндромом." : "Строка не является палиндромом.");
+    }
+}
