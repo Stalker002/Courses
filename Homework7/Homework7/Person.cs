508b6ef470e15171a0a7d19f0c17cca8e387cb90
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2026-01-29 20:22:15 +0300

    Do seventh рщьуцщкл

---
+++ Homework7/Homework7/Person.cs
@@ -0,0 +1,65 @@
+﻿namespace Homework7;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+
+        if (ReferenceEquals(this, obj)) return true;
+
+        if (obj is Person person)
+            return Name == person.Name && Age == person.Age;
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        var key = 82;
+        int res;
+        unchecked
+        {
+            res = key * 23 ^ Name.GetHashCode() ^ Age;
+        }
+
+        return res;
+    }
+
+    public static bool operator ==(Person a, Person b)
+    {
+        if (ReferenceEquals(a, b)) return true;
+        if (a is null || b is null) return false;
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Person a, Person b)
+    {
+        return !(a == b);
+    }
+
+    public static bool operator >(Person a, Person b)
+    {
+        if (ReferenceEquals(a, b)) return false;
+        if (a is null || b is null) return false;
+
+        return a.Age > b.Age;
+    }
+
+    public static bool operator <(Person a, Person b)
+    {
+        if (ReferenceEquals(a, b)) return false;
+        if (a is null || b is null) return false;
+
+        return a.Age < b.Age;
+    }
+}
---
+++ Homework7/Homework7/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var p1 = new Person { Name = "Alice", Age = 23 };
+        var p2 = new Person { Name = "Alice", Age = 23 };
+        var p3 = new Person { Name = "Misha", Age = 21 };
+
+        Console.WriteLine(p1.ToString());
+        Console.WriteLine(p2.ToString());
+        Console.WriteLine(p3.ToString());
+
+        Console.WriteLine();
+
+        Console.WriteLine(p1.Equals(p2));
+        Console.WriteLine(p2.Equals(p3));
+
+        Console.WriteLine();
+
+        Console.WriteLine(p1 == p2);
+        Console.WriteLine(p2 == p3);
+        Console.WriteLine($"Не равны: {p2 != p3}");
+
+        Console.WriteLine();
+
+        Console.WriteLine(p1.GetHashCode());
+        Console.WriteLine(p2.GetHashCode());
+        Console.WriteLine(p3.GetHashCode());
+
+        Console.WriteLine();
+
+        Console.WriteLine($"P1 старше p3: {p1 > p3}");
+        Console.WriteLine($"P3 младше p1: {p3 < p1}");
+    }
+}
